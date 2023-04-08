ae2b7ccae8f8bc07e9ac42fc8a6d1c4e8d301ce7
dradnats1012 <herjebi1012@gmail.com>
2023-04-08 17:04:14 +0900

    [Add] ChangeNumber Class
    Feat : 숫자 변환 클래스 추가

---
+++ src/main/java/baseball/ChangeNumber.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChangeNumber {
+    public static List<Integer> changeUserNumber(String userNumber) {
+        List<Integer> realUserNumber = new ArrayList<>();
+        for (int index = 0; index < userNumber.length(); index++) {
+            realUserNumber.add(parseToInteger(userNumber.charAt(index)));
+        }
+        return realUserNumber;
+    }
+
+    private static int parseToInteger(char ch) {
+        return ch - '0';
+    }
+}
